c17bacf85d81e96733704b450b1b937bc7bf32d5
NickGatti <nick.gatti@gmail.com>
2019-10-19 12:10:38 -0700

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-unused-vars */
+
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Proxy</div>;
+  }
+}
+
+export default App;
---
+++ client/tests/app.test.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line
+import React from "react";
+// eslint-disable-next-line
+import { shallow, mount } from "enzyme";
+import App from "../src/App.jsx";
+
+describe("First React component test with Enzyme", () => {
+  it("works", () => {
+    expect(App).toEqual(App);
+  });
+
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("renders without crashing", () => {
+    mount(<App />);
+  });
+
+  // it("renders the chart without crashing", () => {
+  //   expect(
+  //     mount(<App />)
+  //       .find("ActivityChart")
+  //       .find(".monthly-activity").length
+  //   ).toEqual(1);
+  // });
+});
